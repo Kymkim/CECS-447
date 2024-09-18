5e47e5e216be2bc94ce66ce973315cb67fa90e75
Oliver Cabral <82325590+Kymkim@users.noreply.github.com>
2024-09-18 09:19:54 -0700

    erm

---
+++ Project 2/v2/MC1/LEDSW.c
@@ -0,0 +1,47 @@
+#include "LEDSW.h"
+
+volatile unsigned long HIGH, LOW;
+
+void LEDSW_Init(void){
+  SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF; // (a) activate clock for port F
+	while ((SYSCTL_RCGC2_R &= SYSCTL_RCGC2_GPIOF)==0){};
+		
+  GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY; // 2) unlock PortF PF0  
+  GPIO_PORTF_CR_R |= 0x1F;           // allow changes to PF4,1,0       
+  GPIO_PORTF_DIR_R |= 0x0E;          // (c) make LEDs outputs
+  GPIO_PORTF_DIR_R &= ~0x11;
+  GPIO_PORTF_AFSEL_R &= ~0x1F;       //     disable alt funct 
+  GPIO_PORTF_DEN_R |= 0x1F;          //     enable digital   
+  GPIO_PORTF_PCTL_R &= ~0x000FFFFF; // configure as GPIO
+  GPIO_PORTF_AMSEL_R &= ~0x1F;       //     disable analog functionality on PF
+  GPIO_PORTF_PUR_R |= 0x11;     //     enable weak pull-up on PF0,4
+		
+  GPIO_PORTF_IS_R &= ~0x11;     // (d) PF0,4 is edge-sensitive
+  GPIO_PORTF_IBE_R &= ~0x11;    //     PF0,4 is not both edges
+  GPIO_PORTF_IEV_R |= 0x11;    //     PF4 rising edge event
+  GPIO_PORTF_ICR_R = 0x11;      // (e) clear flag4
+  GPIO_PORTF_IM_R |= 0x11;      // (f) arm interrupt on PF4
+  NVIC_PRI7_R = (NVIC_PRI7_R&~0x00E00000)|0x00800000; // (g) priority 4
+  NVIC_EN0_R |= 0x40000000;      // (h) enable interrupt 30 in NVIC
+		
+	//Setup SysTick for PWM
+	NVIC_ST_CTRL_R = 0;
+	NVIC_ST_RELOAD_R = NVIC_ST_RELOAD_M;
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R&0x1FFFFFFF)|0x40000000;
+	NVIC_ST_CTRL_R |= NVIC_ST_CTRL_CLK_SRC|NVIC_ST_CTRL_INTEN|NVIC_ST_CTRL_ENABLE;
+	
+	//LEDChangeColor(DARK);
+	//LEDChangePWM(50);
+	HIGH = PERIOD * .9;
+	LOW = PERIOD - HIGH;
+
+}
+
+char* LEDGetColorString(){
+	
+}
+
+
+
+
